284b5624bec62031e7897e82f214548cb5392659
diego <diego.maustem@gmail.com>
2024-07-16 16:45:12 -0300

    :sparkles: feat: Criação dos dados da API

---
+++ Authentication-Sanctum/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        \App\Models\User::factory(20)->create();
+        \App\Models\Invoice::factory(20)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
